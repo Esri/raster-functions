593bb9e152cbac99df1e9755136e8b7edd209e6e
Feroz Abdul-Kadar <akferoz@esri.com>
2014-08-26 16:57:31 -0700

    Updated PAF API. Added RFT.

--- Aggregation.py
+++ Aggregation.py
@@ -19,38 +19,37 @@ import numpy as np
 class Aggregation():
     def __init__(self):
         self.name = "Aggregation Function"
-        self.description = "Aggregation Function"
+        self.description = "Aggregates pixel values of a collection of overlapping rasters."
 
-        self.argumentinfo = [
-            {
+    def getParameterInfo(self):
+        return [{
                 "name": "rasters",
-                "datatype": 3,                  # multiple rasters
-                "value": "",
-                "displayname": "Rasters",
-                "required": True
-            },
-        ]
+                "dataType": 3,                                      # multiple rasters
+                "value": None,
+                "displayName": "Rasters",
+                "required": True,
+                "description": "The set of rasters to aggregate.",
+            },]
 
-    def getconfiguration(self, **scalars):
+    def getConfiguration(self, **scalars):
         return {
-          "compositerasters": False,            # don't want to composite all bands of all input rasters
-          "referenceproperties": 2 | 4          # reset any statistics and histogram that might be held by the parent dataset (because this function modifies pixel values). 
+          "referenceProperties": 2 | 4 | 8,                         #
+          "invalidateProperties": 2 | 4                             # 
         }
 
-    def bind(self, **kwargs):
-        kwargs["output_rasterinfo"]["pixeltype"] = "32_BIT_FLOAT"   # output pixels are floating-point values
+    def updateRasterInfo(self, **kwargs):
+        kwargs["output_rasterInfo"]["pixelType"] = "32_BIT_FLOAT"   # output pixels are floating-point values
         return kwargs
 
-    def read(self, **kwargs):
-        inblocks = kwargs["rasters_pixelblock"]                 # get a tuple of pixel blocks where each element is...  
-        n = len(inblocks)                                       # ...a numpy array corresponding to the pixel block of an input raster
+    def updatePixels(self, **pixelBlocks):
+        inBlocks = pixelBlocks["rasters_pixelBlock"]                # get a tuple of pixel blocks where each element is...
+        n = len(inBlocks)                                           # ...a numpy array corresponding to the pixel block of an input raster
         if (n < 1):
           raise Exception("No input rasters provided.")
 
-        outblock = np.array(inblocks[0], dtype="float")         # initialize output pixel block with the first input block
-        for i in range(1, n):                                   # add each subsequent input block to our local output array
-          block = np.array(inblocks[i], dtype="float")
-          outblock = np.add(outblock, block)
+        outBlock = np.array(inBlocks[0], dtype="float")             # initialize output pixel block with the first input block
+        for i in range(1, n):                                       # add each subsequent input block to our local output array
+          outBlock = outBlock + np.array(inBlocks[i], dtype="float")
 
-        np.copyto(kwargs["output_pixelblock"], outblock, casting="unsafe")  # copy local array to output pixel block.  
-        return kwargs
+        np.copyto(pixelBlocks["output_pixelBlock"], outBlock, casting="unsafe")  # copy local array to output pixel block.
+        return pixelBlocks
