0368ee5f6dab32e92cc9bcabb035c960ffcf4bc1
Feroz Abdul-Kadar <akferoz@esri.com>
2014-08-26 17:12:41 -0700

    Interim commit.

--- ReferenceTemplate.py
+++ ReferenceTemplate.py
@@ -21,58 +21,59 @@ import numpy as np
 ### As a raster function it doesn't do anything useful. 
 ### Feel free to use this template a starting point for your implementations or as a cheat-sheet. 
 
-class ReferenceTemplate():
-    def __init__(self):
-        self.name = "Reference Template Function"
-        self.description = "Reference Template Function"
+class ReferenceFunction():                                                  # Class name defaults to module name unless specified in the Python Adapter Function property page
+    def __init__(self):                                                     # Intialize your class' attributes here.
+        self.name = "Reference"                                             # ... this is a short name for the function. Traditionally named "<something> Function".
+        self.description = "Reference Function"                             # ... a friendly detailed description of what this function does. 
 
-        self.argumentinfo = [
-            {
-                "name": "some_numeric_argument",
-                "datatype": 0,
+    def getParameterInfo(self):                                             # Return information on each parameter to your function here as a list of dictionaries. 
+        return [                                                            # ...each entry in the list corresponds to an input parameter
+            {                                                               # ...the dictionary contains attributes that further describe aspects of a parameter
+                "name": "some_numeric_argument",                            # ...the name 
+                "dataType": 0,
                 "value": 100,
-                "displayname": "Friendly Name A",
+                "displayName": "Friendly Name A",
                 "description": "Description of some_numeric_argument",
                 "required": True                        # True or False
             },
             {
                 "name": "some_text_argument",
-                "datatype": 1,
+                "dataType": 1,
                 "value": "default_value",
-                "displayname": "Friendly Name B",
+                "displayName": "Friendly Name B",
                 "description": "Description of some_text_argument",
                 "required": True
             },
             {
                 "name": "raster_argument",
-                "datatype": 2,
+                "dataType": 2,
                 "value": "",
-                "displayname": "Friendly Name C",
+                "displayName": "Friendly Name C",
                 "description": "Description of raster_argument",
                 "required": True
             },
             {
                 "name": "raster_collection_argument",
-                "datatype": 3,
+                "dataType": 3,
                 "value": "",
-                "displayname": "Friendly Name D",
+                "displayName": "Friendly Name D",
                 "description": "Description of raster_collection_argument",
                 "required": True
             },
         ]
 
-    def getconfiguration(self, **scalars):
-        # scalars
+    def getConfiguration(self, **scalars):
         return {
-          "extractbandids": (scalars["red"], scalars["ir"]),        # extract only the two bands corresponding to user-specified red and infrared band indexes.
-          "compositerasters": False,                                # input is a single raster, band compositing doesn't apply.
-          "referenceproperties": 2 | 4                              # reset any statistics and histogram that might be held by the parent dataset (because this function modifies pixel values). 
+          "extractBands": (scalars["red"], scalars["ir"]),        # extract only the two bands corresponding to user-specified red and infrared band indexes.
+          "compositeRasters": False,                                # input is a single raster, band compositing doesn't apply.
+          "referenceProperties": 2 | 4,                              # reset any statistics and histogram that might be held by the parent dataset (because this function modifies pixel values). 
+          "invalidateProperties": 2 | 4                              # reset any statistics and histogram that might be held by the parent dataset (because this function modifies pixel values). 
         }
 
-    def bind(self, **kwargs):
-        kwargs["output_rasterinfo"]["bandcount"] = 1            # output is a single band raster
-        kwargs["output_rasterinfo"]["pixeltype"] = "32_BIT_FLOAT"   # ... with floating-point pixel values.
-        kwargs["output_rasterinfo"]["statistics"] = ({"minimum": 0.0, "maximum": 200.0}, )  # we know a little about the stats of the outgoing raster (scaled NDVI). 
+    def updateRasterInfo(self, **kwargs):
+        kwargs["output_rasterInfo"]["bandCount"] = 1            # output is a single band raster
+        kwargs["output_rasterInfo"]["pixelType"] = "32_BIT_FLOAT"   # ... with floating-point pixel values.
+        kwargs["output_rasterInfo"]["statistics"] = ({"minimum": 0.0, "maximum": 200.0}, )  # we know a little about the stats of the outgoing raster (scaled NDVI). 
 
         #"rasterattributetable"         // Tuple(String + Tuple(Strings : Tuple containing path to the attribute table and field names.
         #"bandcount"                    // Int
@@ -107,30 +108,22 @@ class ReferenceTemplate():
 
         return kwargs
 
-    def read(self, **kwargs):
-        inblock = kwargs["raster_pixelblock"]                   # get the input raster pixel block.
+    def updatePixels(self, **pixelBlocks):
+        inblock = pixelBlocks["raster_pixelBlock"]                   # get the input raster pixel block.
         red = np.array(inblock[0], dtype="float")               # extractbandids ensures first band is Red.
         ir = np.array(inblock[1], dtype="float")                # extractbandids ensures second band is Infrared.
 
         np.seterr(divide="ignore")
 
         outblock = np.multiply(np.divide(ir - red, ir + red), 100.0) + 100.0            # scale and offset the NDVI value (ir-red)/(ir+red).
-        np.copyto(kwargs["output_pixelblock"], outblock, casting="unsafe")              # copy local array to output pixel block.
-        return kwargs
+        np.copyto(pixelBlocks["output_pixelBlock"], outblock, casting="unsafe")              # copy local array to output pixel block.
+        return pixelBlocks
 
-    def getproperty(self, name, defaultvalue, **kwargs):
-        if name.lower == "datatype":
-            return "Processed"
-        else:
-            return defaultvalue
-
-    def getallproperties(self, **args):
-        args["datatype"] = "Processed"                          # outgoing raster is now 'Processed'.
-        args["cloudcover"] = 50.0
-        return args
-
-    def getbandproperty(self, name, bandindex, defaultvalue, **kwargs):
-        return defaultvalue
-
-    def getallbandproperties(self, bandindex, **args):
-        return args
+    def updateKeyMetadata(self, names, bandIndex, **keyMetadata):
+        if bandIndex == -1:
+            keyMetadata["datatype"] = "Processed"               # outgoing raster is now 'Processed'
+        elif bandIndex == 0:
+            keyMetadata["wavelengthmin"] = None                 # reset inapplicable band-specific key metadata 
+            keyMetadata["wavelengthmax"] = None
+            keyMetadata["bandname"] = "NDVI"
+        return keyMetadata
