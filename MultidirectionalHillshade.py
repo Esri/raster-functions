819149848c1a50fbb60a6de1cc8beadcaf2bad89
Feroz Abdul-Kadar <akferoz@esri.com>
2014-09-05 13:04:33 -0700

    Use weighted-kernel for gradient computation. Enable support for z-factor

--- MultidirectionalHillshade.py
+++ MultidirectionalHillshade.py
@@ -1,9 +1,8 @@
+from scipy import ndimage
 import numpy as np
 import math 
 
-
-# TODO: Enable support for padding
-# TODO: Enable different flavors of z-factor
+# TODO: Enable support for scale-dependent flavors of z-factor
 
 class MultidirectionalHillshade():
 
@@ -12,12 +11,16 @@ class MultidirectionalHillshade():
         self.name = "Multidirectional Hillshade Function"
         self.description = ""
 
-        self.isMD = True
+        self.isMDH = True
+        self.zf = 1.0
+
         self.trigLookup = None
-        self.azimuths   = (315.0, 270.0, 225.0, 360.0, 180.0,   0.0)
-        self.elevations = ( 60.0,  60.0, 60.0,   60.0,  60.0,   0.0)
-        self.weights    = (0.167, 0.278, 0.167, 0.111, 0.056, 0.222)
+        self.azimuths   = (315.0, 315.0, 270.0, 225.0, 360.0, 180.0,   0.0)
+        self.elevations = (45.0,   60.0,  60.0, 60.0,   60.0,  60.0,   0.0)
+        self.weights    = (0.00,  0.167, 0.278, 0.167, 0.111, 0.056, 0.222)
         self.factors    = ()
+        self.kx         = [[1, 0, -1], [2, 0, -2], [1, 0, -1]]
+        self.ky         = [[1, 2, 1], [0, 0, 0], [-1, -2, -1]]
 
 
     def getParameterInfo(self):
@@ -31,12 +34,20 @@ class MultidirectionalHillshade():
                 'description': "The primary input raster where pixel values represent elevation.",
             },
             {
-                'name': 'md',
+                'name': 'mdh',
                 'dataType': 'boolean',
-                'value': self.isMD,
+                'value': self.isMDH,
                 'required': True,
                 'displayName': "Multidirectional",
                 'description': "Indicates whether a multidirectional hillshade is generated.",
+            },
+            {
+                'name': 'zf',
+                'dataType': 'numeric',
+                'value': self.zf,
+                'required': False,
+                'displayName': "Z Factor",
+                'description': "The multiplicative factor that converts elevation values to the units of the horizontal (xy-) coordinate system.",
             },
         ]
 
@@ -52,12 +63,15 @@ class MultidirectionalHillshade():
 
 
     def updateRasterInfo(self, **kwargs):
-        if kwargs.has_key('md'):
-            self.isMD = kwargs['md']
+        if kwargs.has_key('mdh'):
+            self.isMDH = kwargs['mdh']
+
+        if kwargs.has_key('zf'):
+            self.zf = kwargs['zf']
 
         kwargs['output_info']['bandCount'] = 1
         kwargs['output_info']['pixelType'] = 'u1'
-        kwargs['output_info']['statistics'] = ()
+        kwargs['output_info']['statistics'] = ({'minimum': 0.0, 'maximum': 255.0}, )
         kwargs['output_info']['histogram'] = ()
         kwargs['output_info']['colormap'] = ()
 
@@ -65,13 +79,13 @@ class MultidirectionalHillshade():
             raise Exception("Input raster has more than one band. Only single-band raster datasets are supported")
 
         cellSize = kwargs['raster_info']['cellSize']
-        self.factors = 1.0 / (8 * cellSize[0]), 1.0 / (8 * cellSize[1])
+        self.factors = self.zf / (8 * cellSize[0]), self.zf / (8 * cellSize[1])
 
         n = len(self.azimuths)
-        self.trigLookup = np.ndarray([n, 3])                        # pre-compute for use in .getHillshade() via .updatePixels()
+        self.trigLookup = np.ndarray([n, 3])                    # pre-compute for use in .getHillshade() via .updatePixels()
         for i in range(0, n - 1):
-            Z = (90.0 - self.elevations[i]) * math.pi / 180.0       # Sun Zenith Angle in Radians
-            A = (90.0 - self.azimuths[i]) * math.pi / 180.0         # Sun Azimuth arithmetic angle (radians)
+            Z = (90.0 - self.elevations[i]) * math.pi / 180.0   # solar _zenith_ angle in radians
+            A = (90.0 - self.azimuths[i]) * math.pi / 180.0     # solar azimuth _arithmetic_ angle in radians
             sinZ = math.sin(Z)
             self.trigLookup[i, 0] = math.cos(Z)
             self.trigLookup[i, 1] = sinZ * math.sin(A)
@@ -81,22 +95,22 @@ class MultidirectionalHillshade():
 
 
     def updatePixels(self, tlc, shape, props, **pixelBlocks):
-        # cf: http://docs.scipy.org/doc/numpy/reference/arrays.indexing.html
         v = np.array(pixelBlocks['raster_pixels'], dtype='f4')
         m = np.array(pixelBlocks['raster_mask'], dtype='u1')
 
-        dx, dy = np.gradient(v)
-        dx = dx * self.factors[0]
-        dy = dy * self.factors[1]
+        dx = ndimage.convolve(v, self.kx) * self.factors[0]
+        dy = ndimage.convolve(v, self.ky) * self.factors[1]
 
-        if self.isMD:
-            outBlock = self.weights[0] * self.getHillshade(v, 0, dx, dy)
-            for i in range(1, 5):
+        if self.isMDH:
+            outBlock = self.weights[1] * self.getHillshade(v, 1, dx, dy)
+            for i in range(2, 6):
                 outBlock = outBlock + (self.weights[i] * self.getHillshade(v, i, dx, dy))
         else:
             outBlock = self.getHillshade(v, 0, dx, dy)
-
+        
         pixelBlocks['output_pixels'] = outBlock[1:-1, 1:-1].astype(props['pixelType'])
+
+        # cf: http://docs.scipy.org/doc/numpy/reference/arrays.indexing.html
         pixelBlocks['output_mask'] = \
             m[:-2,:-2]  & m[1:-1,:-2]  & m[2:,:-2]  \
           & m[:-2,1:-1] & m[1:-1,1:-1] & m[2:,1:-1] \
