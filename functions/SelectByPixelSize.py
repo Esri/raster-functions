ff4623e804d49ae25411595eafb43b64c90ff75c
Feroz Abdul-Kadar <akferoz@esri.com>
2014-09-25 00:57:59 -0700

    Interim. Fix issue with blank output

--- functions/SelectByPixelSize.py
+++ functions/SelectByPixelSize.py
@@ -1,4 +1,5 @@
 import numpy as np
+import ctypes
 
 
 class SelectByPixelSize():
@@ -7,7 +8,11 @@ class SelectByPixelSize():
         self.name = "Select by Pixel Size"
         self.description = "This function returns pixels associated with one of two input rasters based on the request resolution."
         self.threshold = 0.0
+        self.inBands1, self.inBands2, self.outBands = 1, 1, 1
+        self.emit = ctypes.windll.kernel32.OutputDebugStringA
+        self.emit.argtypes = [ctypes.c_char_p]
 
+        
     def getParameterInfo(self):
         return [
             {
@@ -38,10 +43,26 @@ class SelectByPixelSize():
 
 
     def getConfiguration(self, **scalars):
-        self.threshold = scalars.get('threshold', 0.0)
-        return { 'inputMask': True }
+        return { 
+            'inputMask': True 
+        }
 
+        
+    def updateRasterInfo(self, **kwargs):
+        self.threshold = kwargs.get('threshold', 0.0)
+        if self.threshold <= 0.0:
+            self.threshold = np.mean((np.mean(kwargs['r1_info']['cellSize']), np.mean(kwargs['r2_info']['cellSize'])))
 
+        self.inBands1 = kwargs['r1_info']['bandCount']
+        self.inBands2 = kwargs['r2_info']['bandCount']
+        kwargs['output_info']['bandCount'] = min(self.inBands1, self.inBands2)
+        kwargs['output_info']['resampling'] = True
+
+        self.emit("Trace|Threshold cell-size|{0}\n".format(self.threshold))
+        self.emit("Trace|output_info|{0}\n".format(kwargs['output_info']))
+        return kwargs
+        
+        
     def selectRasters(self, tlc, shape, props):
         cellSize = props['cellSize']
         v = 0.5 * (cellSize[0] + cellSize[1])
@@ -53,15 +74,25 @@ class SelectByPixelSize():
     def updatePixels(self, tlc, shape, props, **pixelBlocks):
         cellSize = props['cellSize']
         v = 0.5 * (cellSize[0] + cellSize[1])
-
-        raise Exception("{0}".format(v))
-
+        self.emit("Trace|Request cell-size|{0}\n".format(v))
+        
         if v < self.threshold:
-            pixelBlocks['output_pixels'] = pixelBlocks['r1_pixels'].astype(props['pixelType'])
-            pixelBlocks['output_mask'] = pixelBlocks['r1_mask'].astype('u1')
-        else: 
-            pixelBlocks['output_pixels'] = pixelBlocks['r2_pixels'].astype(props['pixelType'])
-            pixelBlocks['output_mask'] = pixelBlocks['r2_mask'].astype('u1')
+            sPixels = 'r1_pixels'
+            sMask = 'r1_mask'
+            nBands = self.inBands1
+        else:
+            sPixels = 'r2_pixels'
+            sMask = 'r2_mask'
+            nBands = self.inBands2
 
+        if self.outBands == nBands:
+            p = pixelBlocks[sPixels]
+            m = pixelBlocks[sMask]
+        else:
+            p = pixelBlocks[sPixels][0:self.outBands, :, :]
+            m = pixelBlocks[sMask][0:self.outBands, :, :]
+
+        pixelBlocks['output_pixels'] = p.astype(props['pixelType'])
+        pixelBlocks['output_mask'] = m.astype('u1')
         return pixelBlocks
 
