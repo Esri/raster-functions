ba7ece53f3b262527a477e2a4da0ddf5c2665e68
Feroz Abdul-Kadar <akferoz@esri.com>
2014-09-26 09:42:45 -0700

    WIP: Focal statistics with sampling

---
+++ functions/FocalStatistics.py
@@ -0,0 +1,57 @@
+import numpy as np
+import ctypes
+
+
+class FocalStatistics():
+
+    def __init__(self):
+        self.name = "Focal Statistics"
+        self.description = ""
+        self.factor = 1.0
+        
+    def getParameterInfo(self):
+        return [
+            {
+                'name': 'raster',
+                'dataType': 'raster',
+                'value': None,
+                'required': True,
+                'displayName': "Input Raster",
+                'description': ""
+            },
+            {
+                'name': 'factor',
+                'dataType': 'numeric',
+                'value': 1.0,
+                'required': True,
+                'displayName': "Sampling Factor",
+                'description': ""
+            },
+        ]
+
+
+    def getConfiguration(self, **scalars):
+        self.factor = scalars.get('factor', 1.0)
+        return { 
+            'samplingFactor': self.factor,
+            'inputMask': True 
+        }
+
+        
+    def updateRasterInfo(self, **kwargs):
+        kwargs['output_info']['resampling'] = True
+        kwargs['output_info']['cellSize'] = tuple(np.multiply(kwargs['raster_info']['cellSize'], self.factor))
+        kwargs['output_info']['statistics'] = () 
+        kwargs['output_info']['histogram'] = ()
+        return kwargs
+        
+
+    def updatePixels(self, tlc, shape, props, **pixelBlocks):
+        s = slice(None, None, self.factor)
+        p = pixelBlocks['raster_pixels']
+        m = pixelBlocks['raster_mask']
+
+        pixelBlocks['output_pixels'] = p[s, s].astype(props['pixelType'])
+        pixelBlocks['output_mask'] = m[s, s].astype('u1')
+        return pixelBlocks
+
