76f05889b79c71c8330dddc8c3b6b78efb4e0d37
Feroz Abdul-Kadar <akferoz@esri.com>
2015-02-18 00:18:37 -0800

    interim commit

--- functions/InterpolateMesh.py
+++ functions/InterpolateMesh.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy import ndimage
 from scipy import interpolate
 import utils
+from matplotlib import pyplot as plt
 
 class InterpolateMesh():
 
@@ -56,18 +57,20 @@ class InterpolateMesh():
     def updateRasterInfo(self, **kwargs):
         X, Y, F = kwargs.get('x_pixels', None), kwargs.get('y_pixels', None), kwargs.get('f_pixels', None)
 
-        np.ndarray.ndim
-        if F.ndim == 3: F = F[0, ...]                           # handles single band in the value-raster
+        if F.ndim == 3: F = F[0, ...]       # handles only single band in the value-raster
+        F = np.flipud(F)
+
         if X.ndim != 2 or Y.ndim != 2:
             raise Exception("X- and Y-coordinates must be represented by single-band rasters.")
 
-        X, Y = X[0, ...].squeeze(), Y[..., 0::-1].squeeze()
-        self.trace("Trace|InterpolateMesh.updateRasterInfo|X: {0}|Y: {1}|\n".format(X, Y))
+        X, Y = X[0, ...].squeeze(), Y[..., 0::-1].squeeze()     # flipping Y-vector
         xMin, yMin, xMax, yMax = X[0], Y[0], X[-1], Y[-1]
-        sr = int(kwargs.get('sr', 4326))
         noData = kwargs['f_info'].get('noData', ())
-        noData = noData[0] if len(noData) >= 1 else -9999.0
+        if not noData is None and len(noData) >= 1:
+            noData = noData[0]
+            F[F == noData] = -9999
 
+        sr = int(kwargs.get('sr', 4326))
         kwargs['output_info'] = { 
             'bandCount': 1,
             'pixelType': F.dtype.str,
@@ -83,8 +86,7 @@ class InterpolateMesh():
             'resampling': True
         }
 
-        F[F == noData] = 100
-        self.trace("Trace|InterpolateMesh.updateRasterInfo.2|F: {0}|\n".format(F))
+        self.trace("Trace|InterpolateMesh.updateRasterInfo.1|{0}|\n".format(kwargs))
         self.interpolant = interpolate.RectBivariateSpline(Y, X, F)
         return kwargs
 
@@ -96,16 +98,21 @@ class InterpolateMesh():
     def updatePixels(self, tlc, shape, props, **pixelBlocks):
         nRows, nCols = shape if len(shape) == 2 else shape[1:]
         e = utils.computeMapExtents(tlc, shape, props)
-
         f = self.interpolant(np.linspace(e[1], e[3], nRows), np.linspace(e[0], e[2], nCols))
-        pixelBlocks['output_pixels'] = np.flipud(f).astype(props['pixelType'], copy=False)
+        pixelBlocks['output_pixels'] = f.astype(props['pixelType'], copy=False)
         pixelBlocks['output_mask'] = np.ones(shape, dtype='u1')
+
+        #plt.imshow(f, interpolation='nearest')
+        #plt.show()
+        #self.trace("Trace|InterpolateMesh.updatePixels.1|X: {0}|\n".format(np.linspace(e[0], e[2], nCols)))
+        #self.trace("Trace|InterpolateMesh.updatePixels.1|Y: {0}|\n".format(np.linspace(e[1], e[3], nRows)))
+        #self.trace("Trace|InterpolateMesh.updatePixels.1|F: {0}|\n".format(f))
+
         return pixelBlocks
 
 
     def isLicensed(self, **productInfo): 
-        v = productInfo['major']*1.e+10 + productInfo['minor']*1.e+7 + productInfo['build']
         return {
-            'okToRun': v >= 10*1e+10 + 3.0*1e+7 + 4914,
-            'message': "The python raster function is only compatible with ArcGIS 10.3.1 build 4914 or above",
+            'okToRun': utils.isProductVersionOK(productInfo, 10, 3.1, 4919),
+            'message': "The python raster function is only compatible with ArcGIS 10.3.1 build 4919 or above.",
         }
--- functions/utils.py
+++ functions/utils.py
@@ -14,3 +14,7 @@ def computeMapExtents(tlc, shape, props):
     xMin, yMax = e[0]+tlc[0]*dX, e[3]-tlc[1]*dY                 # top-left corner of request on map
     return (xMin, yMax-nRows*dY, xMin+nCols*dX, yMax)           # extents of request on map
  
+
+def isProductVersionOK(productInfo, major, minor, build): 
+    v = productInfo['major']*1.e+10 + int(0.5+productInfo['minor']*10)*1.e+6 + productInfo['build']
+    return v >= major*1e+10 + minor*1e+7 + build
