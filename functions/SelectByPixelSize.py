4d5bd6754e712e0e8f3de6007e36dd462bc62fc0
Feroz Abdul-Kadar <akferoz@esri.com>
2015-02-25 16:20:06 -0800

    reproject request cell size to SR of input raster

--- functions/SelectByPixelSize.py
+++ functions/SelectByPixelSize.py
@@ -8,8 +8,9 @@ class SelectByPixelSize():
         self.name = "Select by Pixel Size"
         self.description = "This function returns pixels associated with one of two input rasters based on the request resolution."
         self.threshold = 0.0
+        self.sr = None
         self.trace = utils.getTraceFunction()
-        self.projection = utils.Projection()
+        self.proj = utils.Projection()
     
         
     def getParameterInfo(self):
@@ -37,8 +38,9 @@ class SelectByPixelSize():
                 'dataType': 'numeric',
                 'value': 0.0,
                 'required': True,
-                'displayName': "Cell Size Threshold (meters)",
-                'description': "The cell size threshold (in meters) that controls which of the two input rasters contributes pixels to the output."
+                'displayName': "Cell Size Threshold",
+                'description': ("The cell size threshold in the units of input rasters' coordinate system that controls which "
+                                "of the two input rasters contributes pixels to the output.")
             },
         ]
 
@@ -55,47 +57,39 @@ class SelectByPixelSize():
         c1, c2 = ir1['cellSize'], ir2['cellSize']
         self.threshold = kwargs.get('threshold', 0.0)
         if self.threshold <= 0.0:
-            self.threshold = np.mean((np.mean(c1), np.mean(c2)))
+            self.threshold = np.mean((c1, c2))
 
+        self.sr = io['spatialReference']
         io['bandCount'] = min(ir1['bandCount'], ir2['bandCount'])
         io['cellSize'] = (.5*(c1[0]+c2[0]), .5*(c1[1]+c2[1]))
-        io['statistics'] = () 
-        io['histogram'] = ()
         io['resampling'] = True
         return kwargs
         
         
     def selectRasters(self, tlc, shape, props):
-        e, w, h = props['extent'], props['width'], props['height']
-        (x1, x2), (y1, y2) = self.projection.transform(props['spatialReference'], 3857, (e[0], e[2]), (e[1], e[3]))
-        c = .5*(.5*abs(x2-x1) + .5*abs(y2-y1))
+        c = np.mean(utils.computeCellSize(props, self.sr, self.proj))
         return ('r1', ) if c < self.threshold else ('r2', )
                 
 
     def updatePixels(self, tlc, shape, props, **pixelBlocks):
-        self.trace("Trace|SelectByPixelSize.updatePixels.1|request-raster: {0}|\n".format(props))
-        e, w, h = props['extent'], props['width'], props['height']
-        (x1, x2), (y1, y2) = self.projection.transform(props['spatialReference'], 3857, (e[0], e[2]), (e[1], e[3]))
-        c = .5*(.5*abs(x2-x1) + .5*abs(y2-y1))
-
-        rasterId = 1 + int(c >= self.threshold)
+        c = np.mean(utils.computeCellSize(props, self.sr, self.proj))   # cell size in the SR of the input raster
+        rasterId = 1 + int(c >= self.threshold)                         # given c, get raster1 or raster2
         p = pixelBlocks['r{0}_pixels'.format(rasterId)].copy()
         m = pixelBlocks['r{0}_mask'.format(rasterId)].copy()
 
-        iB = 1 if p.ndim == 2 else p.shape[0]
-        oB = 1 if len(shape) == 2 else shape[0]
-        if iB < oB:
+        iB = 1 if p.ndim == 2 else p.shape[0]   # matches the number of bands in the input raster
+        oB = 1 if len(shape) == 2 else shape[0] # matches the number of bands in the output raster
+        if iB < oB:                             # we better have enough to fill the output block
             raise Exception("Number of bands of the request exceed that of the input raster.")
 
-        s = ()
-        if iB > 1: s = (0 if oB == 1 else slice(oB), )
-        s += (slice(None), slice(None))
+        # tuple containing one slice object for each dimension of the input pixel block
+        s = (0 if oB == 1 else slice(oB), ) if iB > 1 else ()
+        s += (slice(None), slice(None))         # every element along the row and column dimension
 
-        self.trace("Trace|SelectByPixelSize.updatePixels|request-pixel-size: {0}|output-shape: {1}|\n".format(c, s))
         pixelBlocks['output_pixels'] = p[s].squeeze().astype(props['pixelType'], copy=False)
         pixelBlocks['output_mask'] = m[s].squeeze().astype('u1', copy=False)
-
-
+        
         self.trace("Trace|SelectByPixelSize.updatePixels.2|input: {0}|output: {1}|\n".format(p[0], pixelBlocks['output_pixels'][0]))
+        self.trace("Trace|SelectByPixelSize.updatePixels|request-pixel-size: {0}|output-shape: {1}|\n".format(c, s))
         return pixelBlocks
 
