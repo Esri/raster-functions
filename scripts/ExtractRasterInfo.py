9ea7dce9e05d16037e3bbb98f65c3f20c8de2abf
Sean Gillies <sean.gillies@gmail.com>
2015-03-14 11:29:08 -0600

    Use enumerate()
    This is exactly what enumerate() is for.

--- scripts/ExtractRasterInfo.py
+++ scripts/ExtractRasterInfo.py
@@ -4,10 +4,7 @@ from glob import glob
 
 print("ObjectID,Raster,NCols,NRows,NBands,PixelType,XMin,YMin,XMax,YMax,SRS")
 
-k = 0
-for f in glob(r"e:\raster-types\Data\Tiles\*.tif"):
+for k, f in enumerate(glob(r"e:\raster-types\Data\Tiles\*.tif"), 1):
     r = sa.Raster(f)
     e = r.extent
-    k = k + 1
     print(",".join((str(k), str(f), str(r.width), str(r.height), str(r.bandCount), str(r.pixelType), str(e.XMin), str(e.YMin), str(e.XMax), str(e.YMax), str(r.spatialReference.factoryCode))))
-
