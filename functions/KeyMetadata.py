597c07f31f0a13e43cdb4a319e0b865ead284910
Feroz Abdul-Kadar <akferoz@esri.com>
2015-05-24 18:20:09 -0700

    Enable support for key metadata as JSON [interim].

--- functions/KeyMetadata.py
+++ functions/KeyMetadata.py
@@ -1,11 +1,14 @@
+import json
+from utils import Trace
+
+
 class KeyMetadata():
 
     def __init__(self):
         self.name = "Key Metadata Function"
         self.description = "Override key metadata in a function chain."
-        self.propertyName = ''
-        self.propertyValue = None
-        self.bandNames = []
+        self.props = None
+        self.trace = Trace()
 
 
     def getParameterInfo(self):
@@ -41,33 +44,65 @@ class KeyMetadata():
                 'displayName': "Band Names",
                 'required': False,
                 'description': "A comma-separated string representing updated band names."
-            }
-        ]
-
+            },
+            {
+                'name': 'json',
+                'dataType': 'string',
+                'value': '',
+                'displayName': "Metadata JSON",
+                'required': False,
+                'description': ""
+            },
+    ]
 
     def getConfiguration(self, **scalars):
         return { 
             'invalidateProperties': 8,          # reset any key properties held by the parent function raster dataset
         }
 
-
     def updateRasterInfo(self, **kwargs):
-        self.propertyName = kwargs.get('property', "")  # remember these user-specified scalar inputs
-        self.propertyValue = kwargs.get('value', "")
+        self.props = json.loads(kwargs.get('json', ""))
+        if self.props is None:
+            self.props = {}
 
-        self.bandNames = []
+        p = kwargs.get('property', "").lower()
+        if len(p) > 0:                          # inject name-value pair into bag of properties
+            self.props[p] = kwargs.get('value', "")
+
+        if not 'bandproperties' in self.props.keys():
+            self.props['bandproperties'] = []   # 
+
+        bandCount = kwargs['raster_info']['bandCount']
+        bandProps = self.props['bandproperties']
+        bandProps.append([{} for k in range(0, bandCount-len(bandProps))])
+
         b = kwargs.get('bands', "").strip()
         if len(b) > 0:
-            self.bandNames = b.split(',')
-
+            k = 0
+            bandNames = b.split(',')
+            for k in range(0, min(len(bandProps), len(bandNames))):
+                bandProps[k]['bandname'] = bandNames[k]
+        
+        self.trace.log("{0}|{1}".format("KeyMetadata.updateRasterInfo", self.props))
         return kwargs
 
-
     def updateKeyMetadata(self, names, bandIndex, **keyMetadata):
-        if bandIndex == -1:
-           if len(self.propertyName) > 0:
-                keyMetadata[self.propertyName] = self.propertyValue
-        elif bandIndex < len(self.bandNames):
-            keyMetadata['bandname'] = self.bandNames[bandIndex]
+        if self.props is None:
+            return keyMetadata
+        
+        props = self.props
+        if bandIndex != -1:
+            if 'bandproperties' not in self.props.keys():
+                return keyMetadata
+            bandProps = self.props['bandproperties']
+            if not bandProps or len(bandProps) < bandIndex + 1:
+                return keyMetadata
+            props = bandProps[bandIndex]
 
-        return keyMetadata
+        for name in names:
+            if name in props.keys():
+                v = props[name]
+                keyMetadata[name] = str(v) if isinstance(v, unicode) else v
+        
+        self.trace.log("{0}|{1}".format("KeyMetadata.updateKeyMetadata", keyMetadata))
+        return keyMetadatafor 
