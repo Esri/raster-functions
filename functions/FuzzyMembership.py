612737632d19c7a6239663262df4c8d319a06ca7
Ronak Sumbaly <rsumbaly@esri.com>
2015-05-26 13:06:22 +0400

    Update FuzzyMembership.py
    Cleanup towards PEP8 Compliance

--- functions/FuzzyMembership.py
+++ functions/FuzzyMembership.py
@@ -1,42 +1,42 @@
 import numpy as np
 import math
 
+
 class FuzzyMembership():
 
     def __init__(self):
         self.name = "Fuzzy Membership Function"
         self.description = "Reclassifies or transforms the input data to a 0 to 1 scale based on the possibility of being a member of a specified set"
-        self.max, self.min = 1. , 1.                          # default values for fuzzy membership functions.
-        self.spreadA, self.spreadB = 0.1 , 5.
-        self.meanM, self.stdM = 1. , 1.
+        self.parA = {'minimum': 1., 'mid': None, 'meanMultipler': 1.}
+        self.parB = {'maximum': 1., 'stdMultipler': 1., 'spreadA': 0.1, 'spreadB': 5.}
 
     def getParameterInfo(self):
         return [
-             {
+            {
                 'name': 'raster',
                 'dataType': 'raster',
                 'value': None,
                 'required': True,
                 'displayName': "Input Raster",
-                'description': ("Fuzzy Membership tool - 0 is assigned to those locations that are definitely not a member of the specified set."
-                                " 1 is assigned to those values that are definitely a member of the specified set,"
-                                " and the entire range of possibilities between 0 and 1 are assigned to some level of possible membership")
+                'description': ("Fuzzy Membership tool - 0 is assigned to those locations that are definitely not a member of the specified set. "
+                                "1 is assigned to those values that are definitely a member of the specified set, "
+                                "and the entire range of possibilities between 0 and 1 are assigned to some level of possible membership.")
             },
             {
-                'name': 'op',
+                'name': 'mode',
                 'dataType': 'string',
                 'value': 'Linear',
                 'required': False,
-                'domain': ('Linear','Gaussian','Small','Large','Near','MSSmall','MSLarge'),
+                'domain': ('Linear', 'Gaussian', 'Small', 'Large', 'Near', 'MSSmall', 'MSLarge'),
                 'displayName': "Fuzzy Membership Type",
-                'description': "Select Fuzzy Membership type."
+                'description': "Fuzzy Membership type."
             },
             {
                 'name': 'par1',
                 'dataType': 'numeric',
                 'value': 0.0,
                 'required': True,
-                'displayName': "Parameter 1",
+                'displayName': "Input Parameter A",
                 'description': "Linear : {minimum value} , Gaussian/Near/Small/Large : {mid point} , MSSmall/MSLarge : {mean multiplier}. Enter 0 for default values."
             },
             {
@@ -44,115 +44,123 @@ class FuzzyMembership():
                 'dataType': 'numeric',
                 'value': 0.0,
                 'required': True,
-                'displayName': "Parameter 2",
-                'description': "Linear : {maximum value} , Gaussian/Near/Small/Large : {spread} , MSSmall/MSLarge : {standard deviation multiplier}. Enter 0 for default values."
+                'displayName': "Input Parameter B",
+                'description': "Linear : {maximum value} , Gaussian/Near/Small/Large : {spread} , MSSmall/MSLarge : {std deviation multiplier}. Enter 0 for default values."
             },
             {
                 'name': 'hedge',
                 'dataType': 'string',
                 'value': 'NONE',
                 'required': False,
-                'domain': ('NONE','SOMEWHAT','VERY'),
+                'domain': ('NONE', 'SOMEWHAT', 'VERY'),
                 'displayName': "Hedge",
                 'description': ("A hedge increases or decreases the fuzzy membership values which modify the meaning of a fuzzy set."
                                 " NONE - No hedge applied."
                                 " SOMEWHAT - The square root of the fuzzy membership function. Increases fuzzy membership functions."
                                 " VERY - The square  of the fuzzy membership function. Decreases fuzzy membership functions.")
             },
-
         ]
-
 
     def getConfiguration(self, **scalars):
         return {
-          'extractBands': (0,),                 # we only need the first band.  Comma after zero ensures it's a tuple.
           'inheritProperties': 2 | 4 | 8,       # inherit everything but the pixel type (1)
           'invalidateProperties': 2 | 4 | 8,    # invalidate these aspects because we are modifying pixel values and updating key properties.
           'padding': 0,                         # no padding of the input pixel block
           'inputMask': False                    # we don't need the input mask in .updatePixels()
         }
 
-
     def updateRasterInfo(self, **kwargs):
+        # output raster information
         kwargs['output_info']['bandCount'] = 1
         kwargs['output_info']['pixelType'] = 'f4'
         kwargs['output_info']['statistics'] = ({'minimum': 0.0, 'maximum': 1.0},)
-        kwargs['output_info']['histogram'] = ()
 
-        self.op = kwargs.get('op', 'Linear').lower()
-        self.hedge = kwargs.get('hedge','NONE')
-        par1 = kwargs.get('par1', 0.0)
-        par2 = kwargs.get('par2', 0.0)
+        self.mode = kwargs['mode'].lower()  # input fuzzy membership mode
+        self.hedge = kwargs['hedge']  # to modify fuzzy membership values
 
+        # statistics of input raster
         stats = kwargs['raster_info']['statistics'][0]
         self.mean, self.std = stats['mean'], stats['standardDeviation']
 
-        ### Value assignment to parameters ###
-        if(par1 == 0.0):
-            self.mid, self.min, self.meanM = (stats['minimum']+stats['maximum'])/2 , stats['minimum'] , 1.0     # default values
+        # assignment of fuzzy membership parameters
+        if kwargs['par1'] != 0.0:
+            self.parA = self.parA.fromkeys(self.parA, kwargs['par1'])
         else:
-            self.min = self.mid = self.meanM = par1
+            self.parA['minimum'] = stats['minimum']
+            self.parA['mid'] = (stats['minimum']+stats['maximum'])/2
 
-        if(par2 != 0.0):
-            self.max = self.spreadA = self.spreadB = self.stdM = par2
+        if kwargs['par2'] != 0.0:
+            self.parB = self.parB.fromkeys(self.parB, kwargs['par2'])
         else:
-            self.max, self.stdM, self.spreadA, self.spreadB = stats['maximum'], 1.0, 0.1, 5.0                  # default values
+            self.parB['maximum'] = stats['maximum']
 
-        ### Check range of parameters ###
-        if ((self.spreadA < 0.01 or self.spreadA > 1) and (self.op == 'gauss' or self.op == 'near')) or \
-           ((self.spreadB < 1 or self.spreadB > 10) and (self.op == 'large' or self.op == 'small'))  :
-            raise Exception ("Spread value out of range.")
-        if ((self.min == self.max) and (self.op == "linear")):
-            raise Exception ("Linear minimum and maximum must be different.")
+        # check range of input range
+        # linear fuzzy membership min - max
+        if ((self.parA['minimum'] == self.parB['maximum']) and (self.mode == "linear")):
+            raise Exception("Linear minimum and maximum must be different.")
+
+        # spread values for fuzzy membership function
+        if ((self.parB['spreadA'] < 0.01 or self.parB['spreadA'] > 1) and (self.mode == 'gauss' or self.mode == 'near')) or \
+           ((self.parB['spreadB'] < 1 or self.parB['spreadB'] > 10) and (self.mode == 'large' or self.mode == 'small')):
+            raise Exception("Spread value out of range.")
 
         return kwargs
 
-
     def updatePixels(self, tlc, shape, props, **pixelBlocks):
-        r = np.array(pixelBlocks['raster_pixels'], dtype='f8', copy=False)
+        # get the input raster pixel block
+        raster = np.array(pixelBlocks['raster_pixels'], dtype='f8', copy=False)
 
-        if self.op == 'linear':
-            r = (r - self.min) / (self.max - self.min)             # fuzzy linear membership.
+        # fuzzy linear membership
+        if self.mode == "linear":
+            r = (r - self.parA['minimum']) / (self.parB['maximum'] - self.parA['minimum'])
 
-        elif self.op == 'gaussian':
-            r = (np.e)**((-self.spreadA)*((r-self.mid)**2))        # fuzzy gaussian membership.
+        # fuzzy gaussian membership.
+        elif self.mode == 'gaussian':
+            r = (np.e)**((-self.parB['spreadA']) * ((r - self.parA['mid'])**2))
 
-        elif self.op == 'large':
-            r = (1/(1+((r/self.mid )**(-self.spreadB))))           # fuzzy large membership.
+        # fuzzy large membership.
+        elif self.mode == 'large':
+            r = (1 / (1 + ((r / self.parA['mid'])**(-self.parB['spreadB']))))
 
-        elif self.op == 'small':
-            r = (1/(1+((r/self.mid)**(self.spreadB))))             # fuzzy small membership.
+        # fuzzy small membership.
+        elif self.mode == 'small':
+            r = (1 / (1 + ((r / self.parA['mid'])**(self.parB['spreadB']))))
 
-        elif self.op == 'near':
-            r = (1/(1+(self.spreadA*(r-self.mid)**2)))             # fuzzy near membership.
+        # fuzzy near membership.
+        elif self.mode == 'near':
+            r = (1 / (1 + (self.parB['spreadA'] * (r - self.parA['mid'])**2)))
 
-        elif self.op == 'mssmall':                                 # fuzzy mssmall membership.
-            rtemp = (self.stdM * self.std) / (r - (self.meanM * self.mean) + (self.stdM * self.std))
-            np.putmask(r, r <=(self.mean*self.meanM), 1.0)
-            np.putmask(r, r >(self.mean*self.meanM),rtemp)
+        # fuzzy mssmall membership.
+        elif self.mode == 'mssmall':
+            rTemp = (self.parB['stdMultipler'] * self.std) / (r - (self.parA['meanMultipler'] * self.mean) + (self.parB['stdMultipler'] * self.std))
+            np.putmask(r, r <= (self.mean * self.parA['meanMultipler']), 1.0)
+            np.putmask(r, r > (self.mean * self.parA['meanMultipler']), rTemp)
 
-        else:                                                      # fuzzy mslarge membership.
-            rtemp = 1- (self.stdM * self.std) / (r - (self.meanM * self.mean) + (self.stdM * self.std))
-            np.putmask(r, r <=(self.mean*self.meanM), 0.0)
-            np.putmask(r, r >(self.mean*self.meanM),rtemp)
+        # fuzzy mslarge membership.
+        else:
+            rTemp = 1 - (self.parB['stdMultipler'] * self.std) / (r - (self.parA['meanMultipler'] * self.mean) + (self.parB['stdMultipler'] * self.std))
+            np.putmask(r, r <= (self.mean * self.parA['meanMultipler']), 0.0)
+            np.putmask(r, r > (self.mean * self.parA['meanMultipler']), rTemp)
 
-        np.putmask(r, r < 0.0, 0.0)
-        np.putmask(r, r > 1.0, 1.0)
+        # clip output values between [0.0, 1.0]
+        r = np.clip(r, 0.0, 1.0)
 
-        if (self.hedge == "SOMEWHAT"):      # hedge calculations
-            r = r ** 0.5
-        elif (self.hedge == "VERY"):
-            r = r ** 2
+        # hedge calculations
+        if (self.hedge == "SOMEWHAT"):  r = r ** 0.5
+        elif (self.hedge == "VERY"):    r = r ** 2
 
-        pixelBlocks['output_pixels'] = r.astype(props['pixelType'], copy=False)        # single band output raster
+        if len(r.shape) > 2:
+            pixelBlocks['output_pixels'] = r[0].astype(props['pixelType'], copy=False)     # multi band raster
+        else:
+            pixelBlocks['output_pixels'] = r.astype(props['pixelType'], copy=False)        # single band raster
 
         return pixelBlocks
 
     def updateKeyMetadata(self, names, bandIndex, **keyMetadata):
-       if bandIndex == -1:
+        if bandIndex == -1:
             keyMetadata['datatype'] = 'Scientific'
             keyMetadata['variable'] = 'FuzzyMembership'
-       elif bandIndex == 0:
+        elif bandIndex == 0:
             keyMetadata['wavelengthmin'] = None                 # reset inapplicable band-specific key metadata
             keyMetadata['wavelengthmax'] = None
             keyMetadata['bandname'] = 'FuzzyMembership'
